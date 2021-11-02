05b4e4eacb1fb435dc57f0300b53a3e44afb4613
Ronald de Rooij <ronald.de.rooij@wolfpackit.nl>
2021-11-02 16:31:39 +0100

    Add new meetup function

--- src/components/Backdrop.js
+++
@@ -1,9 +0,0 @@
-function Backdrop(props) {
-  // function cancelHandler() {
-  //   props.onCancel()
-  // }
-
-  return <div className='backdrop' onClick={props.onCancel} />
-}
-
-export default Backdrop
