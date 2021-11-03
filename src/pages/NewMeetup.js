f637bae4ada169255569e51a31660b6ac8733213
Ronald de Rooij <ronald.de.rooij@wolfpackit.nl>
2021-11-03 13:47:49 +0100

    Fetching with a loading state

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
+import { useHistory } from 'react-router'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
+  const history = useHistory()
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-d5fa1-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    ).then(() => {
+      history.replace('/')
+    })
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
